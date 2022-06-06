2b6dd0652d7be0757ccc03ba58bfc9d8c9f571aa
jebbajeb <85389455+jebbajeb@users.noreply.github.com>
2022-06-07 00:42:53 +0800

    updated room generation

--- OnShallowWaters/Assets/04 Scripts/02 Dungeon_System/RoomSpawning/RoomListSO.cs
+++ OnShallowWaters/Assets/04 Scripts/02 Dungeon_System/RoomSpawning/RoomListSO.cs
@@ -5,5 +5,6 @@ using System.Collections.Generic;
 [CreateAssetMenu(fileName = "RoomListSO", menuName = "ScriptableObjects/RoomListSO")]
 public class RoomListSO : ScriptableObject
 {
+    public GameObject roomBasic;
     public List<Room> rooms;
 }
--- OnShallowWaters/Assets/04 Scripts/03 Character/PlayerHandler.cs
+++ OnShallowWaters/Assets/04 Scripts/03 Character/PlayerHandler.cs
@@ -1,9 +1,23 @@
 using UnityEngine;
 using NamejebTools.SaveSystem;
+using System.Collections;
 
 public class PlayerHandler : MonoBehaviour
 {
     [SerializeField] private PlayerStats playerStats;
+    private PlayerMovement _playerMovement;
+    
+
+    private void OnDestroy()
+    {
+        RoomSpawner.OnRoomChanged -= ResetPosition;
+    }
+
+    private void Awake()
+    {
+        _playerMovement = GetComponent<PlayerMovement>();
+        RoomSpawner.OnRoomChanged += ResetPosition;
+    }
     
     public void SavePlayer()
     {
@@ -15,5 +29,23 @@ public class PlayerHandler : MonoBehaviour
     {
         PlayerData.Load();
         print(SaveDataMain.Current.PlayerData.PlayerLocation);
+    }
+
+    private void ResetPosition(Transform spawnPoint)
+    {
+        _playerMovement.enabled = false;
+        StartCoroutine(EnableMovement());
+
+        Vector3 spawnPos = spawnPoint.position;
+        transform.position = new Vector3(spawnPos.x, transform.position.y, spawnPos.z);
+
+        Quaternion spawnRot = spawnPoint.rotation;
+        transform.rotation = new Quaternion(spawnRot.x, spawnRot.y, spawnRot.z, spawnRot.w);
+    }
+
+    private IEnumerator EnableMovement()
+    {
+        yield return new WaitForSeconds(1f);
+        _playerMovement.enabled = true;
     }
 }
