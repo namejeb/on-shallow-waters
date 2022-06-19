d45cbb756e1211054ef1b5a2321c23bd79b3323f
jebbajeb <85389455+jebbajeb@users.noreply.github.com>
2022-06-19 18:52:08 +0800

    tested overlay scene method for choosing wep

--- OnShallowWaters/Assets/04 Scripts/11 Boon_System/BoonTrigger.cs
+++ OnShallowWaters/Assets/04 Scripts/11 Boon_System/BoonTrigger.cs
@@ -4,9 +4,9 @@ public class BoonTrigger : MonoBehaviour
 {
     //spawn when last enemy dies
 
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision other)
     {
-        if (other.CompareTag("Player"))
+        if (other.collider.CompareTag("Player"))
         {
             BoonSelection.Instance.RollBoons();
             Destroy(gameObject);
