1397b5896c3a1d77294be702cbeeb5eaa70a762b
SharifZafran <0128023@kdu-online.com>
2022-06-23 18:11:01 +0800

    added attack sequence

--- OnShallowWaters/Assets/04 Scripts/03 Character/DashNAttack.cs
+++ OnShallowWaters/Assets/04 Scripts/03 Character/DashNAttack.cs
@@ -11,6 +11,9 @@ public class DashNAttack : MonoBehaviour
     [SerializeField] private float speed;
 
     [SerializeField] private LayerMask enemyLayer;
+
+    [SerializeField] private int attackSequence = 0;
+    [SerializeField] private float nextAttack;
     
     
     private bool _isDash = false;
@@ -59,7 +62,31 @@ public class DashNAttack : MonoBehaviour
     {
         animator.SetTrigger("Attack");
 
-        
+        //Attack Sequence(What attack/aniamtion it will do)
+        /*
+        if (attackSequence == 0 && Time.time > nextAttack)
+        {
+            playerMovement.enabled = false;
+            animator.SetTrigger("Attack");
+            attackSequence++;
+            nextAttack = Time.time + 1;
+        }
+        else if (attackSequence == 1 && Time.time > nextAttack)
+        {
+            playerMovement.enabled = false;
+            animator.SetTrigger("Attack");
+            attackSequence++;
+            nextAttack = Time.time + 1;
+        }
+        else if (attackSequence == 1 && Time.time > nextAttack)
+        {
+            playerMovement.enabled = false;
+            animator.SetTrigger("Attack");
+            attackSequence = 0;
+            nextAttack = Time.time + 1.5f;
+
+        }
+        */
 
         //temp damage to test WaveSpawner, will remove
         Collider[] enemies = Physics.OverlapSphere(transform.position, 5f, enemyLayer);
