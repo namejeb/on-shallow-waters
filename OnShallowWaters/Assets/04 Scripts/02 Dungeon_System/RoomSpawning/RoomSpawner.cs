54726f74f6e35aa95b606651fdff34ec9b40fb58
jebbajeb <85389455+jebbajeb@users.noreply.github.com>
2022-06-26 19:33:53 +0800

    updated r1-7 prefab

--- OnShallowWaters/Assets/04 Scripts/02 Dungeon_System/RoomSpawning/RoomSpawner.cs
+++ OnShallowWaters/Assets/04 Scripts/02 Dungeon_System/RoomSpawning/RoomSpawner.cs
@@ -121,8 +121,8 @@ public class RoomSpawner : MonoBehaviour
             {
                 roomIndex = UnityEngine.Random.Range(0, westEntranceRooms.Count);
                 room = westEntranceRooms[roomIndex];
-            }
-            room = southEntranceRooms[1];
+            } 
+            // room = southEntranceRooms[1];
         }
         StartCoroutine(SpawnNewRoom(room));
     }
