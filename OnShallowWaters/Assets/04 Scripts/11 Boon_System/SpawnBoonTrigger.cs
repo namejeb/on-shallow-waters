1c33e1d3e4d9488d05aaa0d3d6eea72c10e01cae
jebbajeb <85389455+jebbajeb@users.noreply.github.com>
2022-06-29 03:23:30 +0800

    disabled things

--- OnShallowWaters/Assets/04 Scripts/11 Boon_System/SpawnBoonTrigger.cs
+++ OnShallowWaters/Assets/04 Scripts/11 Boon_System/SpawnBoonTrigger.cs
@@ -15,11 +15,11 @@ public class SpawnBoonTrigger : MonoBehaviour
     public void Spawn()
     {
         PlayerStats playerStats = PlayerHandler.Instance.PlayerStats;
-        Instantiate(boonTrigger.gameObject, playerStats.transform.position + new Vector3(0f, 1f, 3f), Quaternion.identity);
+        Instantiate(boonTrigger.gameObject, playerStats.transform.position + new Vector3(0f, 3f, 3f), Quaternion.identity);
     }
 
     public void SpawnAtPosition(Vector3 position)
     {
-        Instantiate(boonTrigger.gameObject, position, Quaternion.identity);
+        Instantiate(boonTrigger.gameObject, position + new Vector3(0f, 3f, 0f), Quaternion.identity);
     }
 }
