8dc8827402c2b793bb2958f800754ffb82a5f572
jebbajeb <85389455+jebbajeb@users.noreply.github.com>
2022-06-29 04:03:14 +0800

    samammething

--- OnShallowWaters/Assets/04 Scripts/01 Event_System/Start_Pause/MainMenu.cs
+++ OnShallowWaters/Assets/04 Scripts/01 Event_System/Start_Pause/MainMenu.cs
@@ -4,7 +4,8 @@ using UnityEngine.SceneManagement;
 namespace _04_Scripts._01_Event_System.Start_Pause {
     public class MainMenu : MonoBehaviour {
         public void StartGame(){
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+           // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+           SceneManager.LoadScene(1);
         }
 
         public void ExitGame(){
