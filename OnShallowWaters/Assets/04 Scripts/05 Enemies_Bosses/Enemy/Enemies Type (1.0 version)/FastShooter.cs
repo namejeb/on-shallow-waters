c3147ffb99647f792c7ab4af1cb66543c404c3fe
RandomWinter <85387387+RandomWinter@users.noreply.github.com>
2022-07-10 19:53:17 +0800

    A

--- OnShallowWaters/Assets/04 Scripts/05 Enemies_Bosses/Enemy/Enemies Type (1.0 version)/FastShooter.cs
+++ OnShallowWaters/Assets/04 Scripts/05 Enemies_Bosses/Enemy/Enemies Type (1.0 version)/FastShooter.cs
@@ -1,45 +1,95 @@
-using Unity.VisualScripting.Dependencies.NCalc;
+using System;
+using System.Collections;
 using UnityEngine;
 
 namespace _04_Scripts._05_Enemies_Bosses.Enemy.Enemies_Type__1._0_version_ {
-    public sealed class FastShooter : EnemiesCore{
-        public GameObject projectile;
-        public bool fireOnce;
-
-        // agent.SetDestination(puppet.position);
-        // if(dist < attackRange * attackRange){
-        //     agent.stoppingDistance = attackRange;
-        //     behaviour = CoreStage.Attack;
-        // }
+    public sealed class FastShooter : EnemiesCore {
+        [SerializeField] private GameObject projectile;
+        [SerializeField] private float repositioning, timeToFire;
         
-        protected override void Movement(){
+        private EnemiesProjectile _enemiesProjectile;
+        private Transform _transform2;
+        
+        private Quaternion _rotation;
+        private Vector3 _position, _direct;
+
+        public Transform fireSpawn;
+        public float maxDist = 5f;
+        public bool isPrepared;
+        public int bulletFired;
+        
+        //! Walk Circle
+        [SerializeField] private float rotationRadius = 2f, angularSpeed = 2f;
+        public float posX, posZ, angle1;
+
+        private Vector3 _offset;
+
+        protected override void Start(){
+            base.Start();
+            isPrepared = true;
+            _enemiesProjectile = projectile.GetComponent<EnemiesProjectile>();
         }
 
-        protected override void Attack(){
-            RaycastSingle();
+        protected override void Movement(){
+            if (RaycastSingle() && isPrepared){
+                behaviour = CoreStage.Attack;
+                isPrepared = false;
+            } else {
+                RotateMovement();
+            }
+        }
+
+        private IEnumerator Delay(){
+            yield return new WaitForSeconds(repositioning);
+            isPrepared = true;
+        }
+
+        protected override void Attack(){ 
+            (_transform2 = transform).LookAt(puppet);
+            
+            _direct = puppet.position - _transform2.position;
+            _rotation = Quaternion.LookRotation(_direct);
+            Quaternion rotation1 = _rotation;
+            
+            if (Time.time >= timeToFire) {
+                timeToFire = Time.time + 1 / _enemiesProjectile.fireRate;
+                Instantiate(projectile, fireSpawn.position, rotation1);
+                bulletFired += 1;
+            }
+
+            if (bulletFired != 3) return;
+            behaviour = CoreStage.Move;
+            bulletFired = 0;
         }
 
         private bool RaycastSingle(){
-            var transform1 = transform;
-            var position = transform1.position;
-            
-            Vector3 origin = position;
-            Vector3 direction = transform1.forward;
-            float maxDist = 5f;
-
-            Vector3 directionA = puppet.position - position;
-            float x = Random.Range(-5, 5);
-            float y = Random.Range(-5, 5);
-            Vector3 directionB = directionA + new Vector3(x, y, 0);
+            var trans1 = transform;
+            Vector3 origin = trans1.position;
+            Vector3 direction = trans1.forward;
 
             Ray ray = new Ray(origin, direction);
-            bool result = Physics.Raycast(ray, out RaycastHit hitInfo, maxDist);
+            Debug.DrawRay(origin, direction * radius, Color.cyan);
+            
+            bool result = Physics.Raycast(ray, radius, targetMask, QueryTriggerInteraction.Ignore);
+            return result;
+        }
 
-            if (!result || !hitInfo.collider.CompareTag("Player") || fireOnce) return false;
-            GameObject currentBullet = Instantiate(projectile, transform.position, Quaternion.identity);
-            currentBullet.transform.forward = directionB.normalized;
-            fireOnce = true;
-            return true;
+        private void RotateMovement(){
+            var position13 = puppet.position;
+
+            _offset.Set(
+                Mathf.Cos(angle1) * radius,
+                0,
+                Mathf.Sin(angle1) * radius
+            );
+            
+            agent.SetDestination(position13 + _offset);
+            angle1 += Time.deltaTime * angularSpeed;
+            StartCoroutine(Delay());
+
+            if (angle >= 360f){
+                angle = 0f;
+            }
         }
     }
 }
