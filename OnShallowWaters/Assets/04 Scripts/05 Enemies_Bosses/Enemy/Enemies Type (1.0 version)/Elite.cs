a500a5fefbe7f88c9d010dada4ca83ab05222a07
RandomWinter <85387387+RandomWinter@users.noreply.github.com>
2022-07-13 12:38:17 +0800

    ds

--- OnShallowWaters/Assets/04 Scripts/05 Enemies_Bosses/Enemy/Enemies Type (1.0 version)/Elite.cs
+++ OnShallowWaters/Assets/04 Scripts/05 Enemies_Bosses/Enemy/Enemies Type (1.0 version)/Elite.cs
@@ -1,9 +1,7 @@
 
-using System;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
-namespace _04_Scripts._05_Enemies_Bosses.Enemy.Enemies_Type__1._0_version_{
+
     public class Debuffer : EnemiesCore {
         public Transform firePoint;
         public bool fireOnce;
@@ -28,43 +26,19 @@ namespace _04_Scripts._05_Enemies_Bosses.Enemy.Enemies_Type__1._0_version_{
         }
 
         [SerializeField] private float timeToFire;
-        //private BottleAoe _enemiesProjectile;
-        // ball = bottle.GetComponent<Rigidbody>();
-            // ball.useGravity = false;
             
-            protected override void Attack(){
-                Transform trans;
-                (trans = transform).LookAt(puppet);
-                _direction = puppet.position - trans.position;
-                _rotate = Quaternion.LookRotation(_direction);
+        protected override void Attack(){
+            Transform trans;
+            (trans = transform).LookAt(puppet);
+            _direction = puppet.position - trans.position;
+            _rotate = Quaternion.LookRotation(_direction);
                 
                 //RaycastSingle();
-                if (Time.time >= timeToFire) {
-                    timeToFire = Time.time + 1 / 0.5f;
-                    ThrowBottle();
-                }
+            if (Time.time >= timeToFire) {
+                timeToFire = Time.time + 1 / 0.5f;
+                ThrowBottle();
             }
-
-            // private void RaycastSingle(){
-            //      var transform1 = transform;
-            //      var position = transform1.position;
-            //     
-            //     Vector3 origin = position;
-            //     Vector3 direction = transform1.forward;
-            //
-            //     Vector3 directionA = puppet.position - position;
-            //     float x = Random.Range(-5, 5);
-            //     float y = Random.Range(-5, 5);
-            //     Vector3 directionB = directionA + new Vector3(x, y, 0);
-            //
-            //     Ray ray = new Ray(origin, direction);
-            //     bool result = Physics.Raycast(ray, out RaycastHit hitInfo, maxDist);
-            //
-            //     if (!result || !hitInfo.collider.CompareTag("Player") || fireOnce) return;
-            //     fireOnce = true;
-            //
-            //     //Launch();
-            // }
+        }
         
         private void ThrowBottle() {
             Quaternion rotation = _rotate;
@@ -79,14 +53,6 @@ namespace _04_Scripts._05_Enemies_Bosses.Enemy.Enemies_Type__1._0_version_{
             debuffBottle.GetComponent<Rigidbody>().velocity = CalculateLaunchVelocity();
         }
 
-        // void Launch(){
-        //     Instantiate(bottle, firePoint.position, firePoint.rotation);
-        //     Physics.gravity = Vector3.up * gravity;
-        //     ball.useGravity = true;
-        //     ball.velocity = CalculateLaunchVelocity();
-        // }
-        //
-        // //! Sebastian Lague Method for Kinematic Equation
         Vector3 CalculateLaunchVelocity() {
             var position = puppet.position;
             var position1 = firePoint.position;
@@ -103,4 +69,4 @@ namespace _04_Scripts._05_Enemies_Bosses.Enemy.Enemies_Type__1._0_version_{
             return velocityXZ + velocityY;
         }
     }
-}
+
