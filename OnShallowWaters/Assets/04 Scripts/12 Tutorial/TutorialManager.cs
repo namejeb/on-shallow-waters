0c570369809bb1d88256c74dabccfab3865853f6
jebbajeb <85389455+jebbajeb@users.noreply.github.com>
2022-07-27 03:14:09 +0800

    renamed ManagerTutorial.cs to TutorialManager.cs

--- OnShallowWaters/Assets/04 Scripts/12 Tutorial/TutorialManager.cs
+++ OnShallowWaters/Assets/04 Scripts/12 Tutorial/TutorialManager.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ManagerTutorial : MonoBehaviour
+public class TutorialManager : MonoBehaviour
 {
     public enum tutorialStage{
         Intro, Action, End
