bac93af447532e3282e47392ba495c822a21a920
jebbajeb <85389455+jebbajeb@users.noreply.github.com>
2022-08-03 12:45:36 +0800

    added tutorial

--- OnShallowWaters/Assets/04 Scripts/03 Character/Player/DashNAttack.cs
+++ OnShallowWaters/Assets/04 Scripts/03 Character/Player/DashNAttack.cs
@@ -50,7 +50,7 @@ public class DashNAttack : MonoBehaviour
     private BM_DmgWhenArmorBreak _dmgWhenShieldBreak;
 
     //Tutorial Event
-    public static event Action OnAttack;
+    //public static event Action OnAttack;
     public static event Action OnDash;
     
     public static event Action<Transform, float, bool> OnHitLanded;
@@ -92,9 +92,7 @@ public class DashNAttack : MonoBehaviour
     private void Dash()
     {
         animator.SetTrigger("Dash");
-        if (OnDash != null){
-            OnDash();
-        }
+
 
         playerMovement.Move(transform.forward, speed, true);
         if (Time.time > _endTime)
@@ -107,7 +105,9 @@ public class DashNAttack : MonoBehaviour
 
     public void ActivateDash()
     {
-
+        if (OnDash != null){
+            OnDash();
+        }
         dashButton.interactable = false;
         _isDash = true;
         playerMovement.enabled = false;
--- OnShallowWaters/Assets/04 Scripts/13 UI/FaceCamera.cs
+++ OnShallowWaters/Assets/04 Scripts/13 UI/FaceCamera.cs
@@ -1,19 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FaceCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    // void Start()
-    // {
-    //     _cam = Camera.main; 
-    // }
-    //
-    // // Update is called once per frame
-    // void Update()
-    // {
-    //     _camRot = _cam.transform.rotation;
-    //     transform.LookAt(transform.position + _camRot * Vector3.forward, _camRot * Vector3.up);
-    // }
+    private Camera _cam;
+    private Quaternion _camRot;
+
+    void Start()
+    {
+        _cam = Camera.main; 
+    }
+    
+
+    void Update()
+    {
+        _camRot = _cam.transform.rotation;
+        transform.LookAt(transform.position + _camRot * Vector3.forward, _camRot * Vector3.up);
+    }
 }
