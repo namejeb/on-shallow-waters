12229b4a06d12fd63449fa29d01ed322d776cb7d
jebbajeb <85389455+jebbajeb@users.noreply.github.com>
2022-08-14 04:50:29 +0800

    s

--- OnShallowWaters/Assets/04 Scripts/02 Dungeon_System/ExitRoomTrigger.cs
+++ OnShallowWaters/Assets/04 Scripts/02 Dungeon_System/ExitRoomTrigger.cs
@@ -1,35 +1,24 @@
 using UnityEngine;
 using System;
 using UnityEngine.SceneManagement;
-using System.Collections;
 
 
 public class ExitRoomTrigger : MonoBehaviour
 {
     [SerializeField] private RoomEntranceDir nextRoomEntranceDir;
     public static event Action<RoomEntranceDir> OnExitRoom;
+
+    private GameObject _loadingScreen;
+    
     
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            if (GameManager.IsTutorial)
-            {
-                StartCoroutine(LoadMainMenu());
-                return;
-            }
-            
             if (RoomSpawnerV2.IsBossRoom)
                 SceneManager.LoadScene( (int) SceneData.WinScene);
             else
                 if (OnExitRoom != null) OnExitRoom.Invoke(nextRoomEntranceDir);
         }
-    }
-
-    private IEnumerator LoadMainMenu()
-    {
-        RoomSpawnerV2.TriggerTransitionStart();
-        yield return new WaitForSeconds(RoomSpawnerV2.TransitionDuration);
-        SceneManager.LoadScene( (int) SceneData.MainMenu );
     }
 }
