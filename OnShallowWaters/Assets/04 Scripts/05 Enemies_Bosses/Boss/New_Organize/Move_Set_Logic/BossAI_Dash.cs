e2cc9a29ab740c952555b6196574578033100d9a
qiuji <74701224+qiuji10@users.noreply.github.com>
2022-08-19 00:54:47 +0800

    Boss Finale Tuning

--- OnShallowWaters/Assets/04 Scripts/05 Enemies_Bosses/Boss/New_Organize/Move_Set_Logic/BossAI_Dash.cs
+++ OnShallowWaters/Assets/04 Scripts/05 Enemies_Bosses/Boss/New_Organize/Move_Set_Logic/BossAI_Dash.cs
@@ -17,6 +17,7 @@ public class BossAI_Dash : State
 
     public override void EnterState(StateMachineManager sm)
     {
+        sm.inStateTimer = 0;
         sm.Agent.enabled = false;
         sm.Anim.SetTrigger(chargeAnimation);
     }
@@ -36,7 +37,7 @@ public class BossAI_Dash : State
 
         if (sm.inStateTimer > dashStartTime && sm.inStateTimer <= dashTimeout)
         {
-            sm.Anim.SetTrigger(dashAnimation);
+            //sm.Anim.SetTrigger(dashAnimation);
             sm.Rb.isKinematic = false;
             sm.Rb.AddForce(new Vector3(_direction.x, 0, _direction.z) * dashForce);
         }
@@ -44,7 +45,7 @@ public class BossAI_Dash : State
         if (sm.inStateTimer > dashTimeout)
         {
             isDashing = false;
-            sm.Anim.ResetTrigger("isDash");
+            //sm.Anim.ResetTrigger("isDash");
             sm.Anim.SetTrigger("toNormal");
             sm.inStateTimer = 0;
             sm.HitBoxOff(2);
